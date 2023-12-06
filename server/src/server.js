99fb507b32dfb07944cd94244ea3daa65099c03f
Acus <acus1304pgphcm@gmail.com>
2023-12-07 00:40:04 +0700

    Phuc | Create config and .env for MongoDB connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,35 @@
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import asyncExitHook from 'async-exit-hook'
+import { CLOSE_DB, CONNECT_DB } from '~/config/mongodb'
+import 'dotenv/config'
+import { env } from '~/config/environment'
 
-const app = express();
-const hostname = 'localhost';
-const port = 8000;
+const START_SERVER = () => {
+  const app = express();
+  const hostname = env.APP_HOST || "localhost";
+  const port = env.APP_PORT || 8000;
 
-app.get('/', (req, res) => {
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ));
-  res.end('<h1>Hello World!</h1><hr>');
-})
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>');
+  })
+  app.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}/`);
+  })
 
-app.listen(port, hostname, () => {
-
-  console.log(`Server is running at http://${hostname}:${port}/`);
-})
+  // Clean up services before shutting down server
+  asyncExitHook(() => {
+    CLOSE_DB();
+    console.log('Server shuts down!');
+  })
+}
+// IIFE (Immediately Invoked Function Expression)
+(async () => {
+  try {
+    await CONNECT_DB();
+    console.log('Connected to Mongodb Atlas!');
+    START_SERVER();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})();
