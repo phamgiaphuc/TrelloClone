fc724efa4a6aecb9edd6207b42c02fcf961489f5
Acus <acus1304pgphcm@gmail.com>
2023-12-07 22:29:23 +0700

    Phuc | Add cross-env

--- server/src/middlewares/errorHandlingMiddleware.js
+++ server/src/middlewares/errorHandlingMiddleware.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { StatusCodes } from 'http-status-codes'
-// import { env } from '~/config/environment'
+import { env } from '~/config/environment'
 
 export const errorHandlingMiddleware = (err, req, res, next) => {
   if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
@@ -9,8 +9,8 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
     message: err.message || StatusCodes[err.statusCode],
     stack: err.stack
   }
-  // console.error(responseError)
-  // if (env.BUILD_MODE !== 'dev') delete responseError.stack
+  // console.error('env.BUILD_MODE: ', env.BUILD_MODE)
+  if (env.BUILD_MODE !== 'dev') delete responseError.stack
   // console.error(responseError)
   res.status(responseError.statusCode).json(responseError)
 }
