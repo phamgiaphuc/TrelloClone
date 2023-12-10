66b4e127733439d259e0fe6ece41b3b4973c7c0c
Acus <acus1304pgphcm@gmail.com>
2023-12-10 12:44:50 +0700

    Phuc | Done aggregate query

--- server/src/models/boardModel.js
+++ server/src/models/boardModel.js
@@ -1,14 +1,17 @@
 import Joi from 'joi'
 import { ObjectId } from 'mongodb'
 import { GET_DB } from '~/config/mongodb'
+import { BOARD_TYPES } from '~/utils/constants'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+import { columnModel } from './columnModel'
+import { cardModel } from './cardModel'
 
 const BOARD_COLLECTION_NAME = 'boards'
 const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(64).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
-  type: Joi.string().min(3).max(16).trim().strict().default('public').valid('public', 'private'),
+  type: Joi.string().min(3).max(16).trim().strict().default('public').valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE),
   columnOrderIds: Joi.array().items(
     Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
   ).default([]),
@@ -43,9 +46,25 @@ const findOneById = async (id) => {
 
 const getDetails = async (id) => {
   try {
-    return await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: new ObjectId(id)
-    });
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).aggregate([
+      { $match: {
+        _id: new ObjectId(id),
+        _destroy: false
+      } },
+      { $lookup: {
+        from: columnModel.COLUMN_COLLECTION_NAME,
+        localField: '_id',
+        foreignField: 'boardId',
+        as: 'columns'
+      } },
+      { $lookup: {
+        from: cardModel.CARD_COLLECTION_NAME,
+        localField: '_id',
+        foreignField: 'boardId',
+        as: 'cards'
+      } }
+    ]).toArray()
+    return result[0] || {}
   } catch (error) {
     throw new Error(error);
   }
